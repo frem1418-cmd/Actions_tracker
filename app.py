5caa67c8a4dfe72c4f9321da3239043e1142be4f
frem <frem1418@gmail.com>
2026-04-23 07:28:03 +0200

    ajout gestion de colones en fonction du profil

--- app.py
+++ app.py
@@ -712,7 +712,7 @@ if t_list:
             selection_mode="single-row",
             use_container_width=True,
             hide_index=True,
-            height=min(hauteur_dynamique, 900),
+            height=min(hauteur_dynamique, 850),
             column_config=config_colonnes 
         )
         if sel.selection and sel.selection.rows:
