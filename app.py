a5cfde1c709a2c82b450813ddaf50997f6724c2c
frem <frem1418@gmail.com>
2026-04-25 10:28:20 +0200

    optimisation news et mise en place traductions

--- app.py
+++ app.py
@@ -1036,7 +1036,8 @@ if t_list:
                             unique_news.append(article)
                             titres_vus.add(t_brut)
                 
-     
+                query = st.session_state.get("search_news_detail", "") # Si tu as une barre de recherche
+
                 if query:
                     q = query.lower()
                     unique_news = [
