70c396ef08c86450977c1a05285c7dfd799552bf
frem <frem1418@gmail.com>
2026-04-29 22:58:32 +0200

    optimisation news et mise en place traductions

--- app.py
+++ app.py
@@ -101,15 +101,7 @@ def get_quick_news(ticker):
                     parts = e.title.rsplit(' - ', 1)
                     clean_title = parts[0]
                     source_name = parts[1] if len(parts) > 1 else default_source
-                    title_upper = clean_title.upper()
-
-                    # FILTRE ANTI-BRUIT
-                    # On définit les mots interdits pour éviter la confusion L'OREAL / OR
-                    forbidden_keywords = [" l'or ", " GOLD ", " ONCE "] # Espaces importants pour ne pas bloquer "ORDRE"
-                    # On vérifie si le ticker (ex: OR) ou le nom de l'action est dans le titre
-                    if t_clean not in title_upper or any(word in title_upper for word in forbidden_keywords):
-                        continue # On passe à la news suivante sans l'ajouter à news_list
-
+                    
                     # --- ANALYSE SENTIMENT  ---
                     pol = TextBlob(clean_title).sentiment.polarity
                     sentiment_label = "Positif" if pol > 0.1 else "Négatif" if pol < -0.1 else "Neutre"
